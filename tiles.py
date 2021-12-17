119e6c8feeb4fa1d307490f5b79fd26178e72d5a
jorge-junior <91333173+jorge-junior@users.noreply.github.com>
2021-12-16 18:30:20 -0800

    color_change
    adicionei a função que muda as cores dos objetos e uma cor nova, as mudanças estão neste formato
     #\\\\\\\\\
    mudança
    #\\\\\\\\\\
    ps: qualquer duvida, contate pelo zap

--- tiles.py
+++ tiles.py
@@ -1,15 +1,42 @@
+from typing import Tuple
 import pygame as pg
 import os, csv
+# \\\\\\\\\\\\\\\\\\\\\\\\\\
+#Função que muda a cor de um bloco(tbm funciona com poçoes)
+def changeColor(image, color):
+    colouredImage = pg.Surface(image.get_size())
+    colouredImage.fill(color)
+
+    finalImage = image.copy()
+    finalImage.blit(colouredImage, (0, 0), special_flags=pg.BLEND_MIN)
+    return finalImage
+
+# dicionario com cores pra mudar
+hue = {
+    1:(135,90,255),
+    2:(100,50,100) # cor teste
+}
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 class Tile:
-    def __init__(self,image,x,y,solid=False,collectable=False,id=None,color=None):
-        self.image = pg.image.load(image).convert_alpha()
+    def __init__(self,image,x,y,solid=False,collectable=False,id=None,color = None, color_id= None):  # novo parametro color_id
+        # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        # color_id : posicao da cor no dicionario hue
+        # caso seja um bloco colorido
+        if color_id != None:
+            self.color_value =  pg.Color(hue[color_id])
+            self.image = changeColor(pg.image.load(image).convert_alpha(), self.color_value)
+        # outro bloco
+        else:
+            self.image = pg.image.load(image).convert_alpha()
+        # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x,y
         self.solid = solid
         self.collectable = collectable
         self.id = id
         self.color = color
+        
 
     def draw(self, surf):
         surf.blit(self.image,(self.rect.x,self.rect.y))
@@ -60,10 +87,19 @@ class TileMap:
                     tiles.append(Tile('assets/tiles/wallbr.png',x*self.tile_size,y*self.tile_size))
                 if tile == '7':
                     objects.append(Tile('assets/crate.png',x*self.tile_size,y*self.tile_size,solid=True))
+                # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                # tiles abaixo possuem o atributo color_id para modificar sua cor
                 if tile == '8':
-                    objects.append(Tile('assets/crate_purple.png',x*self.tile_size,y*self.tile_size,solid=True,color=(135,90,255)))
+                    objects.append(Tile('assets/crate_purple.png',x*self.tile_size,y*self.tile_size,solid=True,color = (135,90,255), color_id = 1))
+                # bloco teste
+                if tile == '999': 
+                    objects.append(Tile('assets/crate_purple.png',x*self.tile_size,y*self.tile_size,solid=True,color = (100,50,100), color_id = 2))
+                # poção teste
+                if tile == '998': 
+                    objects.append(Tile('assets/potion.png',x*self.tile_size,y*self.tile_size,collectable=True,id='test_potion', color_id = 2))
                 if tile == '17':
-                    objects.append(Tile('assets/potion.png',x*self.tile_size,y*self.tile_size,collectable=True,id='purple_potion'))
+                    objects.append(Tile('assets/potion.png',x*self.tile_size,y*self.tile_size,collectable=True,id='purple_potion', color_id = 1))
+                # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
                 if tile == '10':
                     objects.append(Tile('assets/coin.png',x*self.tile_size,y*self.tile_size,collectable=True,id='coin'))
                 x += 1
